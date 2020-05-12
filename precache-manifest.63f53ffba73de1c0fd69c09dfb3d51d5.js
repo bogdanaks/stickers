a0ddf2550fba7cfe8e15d4346a530214a4081aac
Skeefz <bogdanaks@bk.ru>
2020-05-12 14:55:25 +0300

    Updates

---
+++ precache-manifest.63f53ffba73de1c0fd69c09dfb3d51d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "500e3d5d548f90aaa8533a260152b1be",
+    "url": "/stickers/index.html"
+  },
+  {
+    "revision": "6a27b147372596e946db",
+    "url": "/stickers/static/css/main.e09a7aba.chunk.css"
+  },
+  {
+    "revision": "9fda4c0db1673fb0a774",
+    "url": "/stickers/static/js/2.af686364.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/stickers/static/js/2.af686364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a27b147372596e946db",
+    "url": "/stickers/static/js/main.8dd09407.chunk.js"
+  },
+  {
+    "revision": "14a159f318dda86bbfee",
+    "url": "/stickers/static/js/runtime-main.3d79f733.js"
+  },
+  {
+    "revision": "450b44709623a47ec849b26f77d87c39",
+    "url": "/stickers/static/media/bg.450b4470.jpg"
+  },
+  {
+    "revision": "aeb6d9a4cc0f681a69b925cda7b3c77c",
+    "url": "/stickers/static/media/cancBtn1.aeb6d9a4.png"
+  },
+  {
+    "revision": "f7c9cd5db91077e2f0116e2a497553df",
+    "url": "/stickers/static/media/cancBtn2.f7c9cd5d.png"
+  },
+  {
+    "revision": "0c1d1f1c43189c7307773f0108bd390b",
+    "url": "/stickers/static/media/cancBtn3.0c1d1f1c.png"
+  },
+  {
+    "revision": "44a9a30dc4baf7a7a934adf18aedeb8a",
+    "url": "/stickers/static/media/stickerOne.44a9a30d.png"
+  },
+  {
+    "revision": "aa8dcf35cb527df8b72fcf43e4551c54",
+    "url": "/stickers/static/media/stickersPack.aa8dcf35.png"
+  }
+]);
